9db52fe6372762ebd2f8c96a7f49ec6e40c4dc17
Pablo M1 <thepaulsanz@gmail.com>
2021-10-29 08:57:34 -0300

    clase 1.4 componentes

---
+++ components/VariableControl.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h4>Control de {{titulo}}</h4>
+    <button @click="valor--">Disminuir</button>
+    <span>{{ valor }}</span><span> {{unidad}}</span>
+    <button @click="valor++">Aumentar</button>
+    <br /><br />
+  </div>
+</template>
+
+<script>
+export default {
+    props:["titulo","unidad"],
+    data(){
+        return{
+            valor: 0
+        }
+    }
+};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <button @click="temperatura--">Disminuir</button>
-    <span>{{temperatura}}</span><span> °C</span>
-    <button @click="temperatura++">Aumentar</button>
+    <variable-control titulo="Temperatura" unidad=" °C"></variable-control>
+    <variable-control titulo="Humedad" unidad=" %"></variable-control>
+    <variable-control titulo="Iluminación" unidad=" Lumen"></variable-control>
+<variable-control titulo="Caudal" unidad=" Litros"></variable-control>
+
   </div>
 </template>
 
 <script>
+import VariableControl from '~/components/VariableControl.vue'
   export default {
+  components: { VariableControl },
     data(){
       return {
         temperatura: 10,
         humedad: 0,
+        iluminacion: 0,
       }
     }
   }
