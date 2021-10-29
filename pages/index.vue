9b5ea54dc644613eb8d300779a269a56552fb39c
Pablo M1 <thepaulsanz@gmail.com>
2021-10-29 08:28:15 -0300

    clase 1.3 imprimiendo variables, alterando valores

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <Tutorial/>
+  <div>
+    <button @click="temperatura--">Disminuir</button>
+    <span>{{temperatura}}</span><span> °C</span>
+    <button @click="temperatura++">Aumentar</button>
+  </div>
 </template>
 
 <script>
-export default {}
+  export default {
+    data(){
+      return {
+        temperatura: 10,
+        humedad: 0,
+      }
+    }
+  }
 </script>
