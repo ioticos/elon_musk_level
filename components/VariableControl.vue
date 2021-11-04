ff35f9dbef0073e614db080a83656e0741482588
pablo <doupanda.com@gmail.com>
2021-11-04 13:17:21 -0300

    clase 1.22 pasando datos de padre a componente hijo

--- components/VariableControl.vue
+++ components/VariableControl.vue
@@ -3,14 +3,24 @@
     <h4>Control de {{titulo}}</h4>
     <button @click="valor--">Disminuir</button>
     <span>{{ valor }}</span><span> {{unidad}}</span>
-    <button @click="valor++">Aumentar</button>
+    <button :disabled="valor >= max"  @click="valor++">Aumentar</button>
     <br /><br />
   </div>
 </template>
 
 <script>
 export default {
-    props:["titulo","unidad"],
+    props:{
+      max:{},
+      titulo: {
+        type: String,
+        required: true,
+        description: "Aqui se pasará el título que el componente mostrará." 
+      },
+      unidad: {
+        type: String
+      }
+    },
     data(){
         return{
             valor: 0
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,24 @@
 <template>
   <div>
-
-
-
-    <h4>Control de Temperatura</h4>
-    <button @click="temperatura--">Disminuir</button>
-    <span 
-      :class="{
-        warning : temperatura > 20 && temperatura < 25, 
-        danger : temperatura >= 25}" >
-        
-        {{temperatura}}</span><span> °C</span>
-    <button @click="temperatura++">Aumentar</button>
-    <br>
-    <h5 v-if="temperatura > 20" style="color:orange">Cuidado!</h5>
-    <h5 v-show="temperatura > 25" style="color:red">Mucho Cuidado!</h5>
-    <br><br>
-
-    <h4>Control de humedad</h4>
-    <button @click="humedad--">Disminuir</button>
-    <span class="tipografia">{{humedad}}</span><span> %</span>
-    <button @click="humedad++">Aumentar</button>
-    <br><br>
     
+    <variable-control :titulo="cuenta" unidad="°C" :max="10"></variable-control>
+  
+    <variable-control :titulo="cuenta * 10" unidad="%" :max="3"></variable-control>
+  
+  <button @click="cuenta++">SUMA 1</button>
+
   </div>
 </template>
 
 <script>
+import VariableControl from '~/components/VariableControl.vue'
   export default {
+  components: { VariableControl },
     data(){
       return {
         temperatura: 13,
-        humedad: 0
+        humedad: 0,
+        cuenta: 0
       }
     }
   }
