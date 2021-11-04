92b2f1f0792865ae22be2683c3dae3fa3a0d7ea8
pablo <doupanda.com@gmail.com>
2021-11-04 12:37:38 -0300

    clase 1.21 nuxt-link y spa

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/escuela">Escuela</nuxt-link>
+      <nuxt-link to="/carrito">Tienda</nuxt-link>
+    </nav>
+
+        <nuxt></nuxt>
+
+    <h5 style="color:red">ENIAC La mejor escuela...</h5>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
